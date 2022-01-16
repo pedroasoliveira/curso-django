c579ee6b6b4d3811a2c2996dc8c00240fb2621c0
pedro <pedroaugusto.ufc@gmail..com>
2022-01-16 16:50:32 -0300

    Configuração do SENTRY (monitoramento de erros).

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Melissa, te amo!!")
+    raise ValueError()
+    return HttpResponse('<html><body>Melissa, te amo!!</body></html>')
