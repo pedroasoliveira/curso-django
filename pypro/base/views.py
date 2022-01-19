67bfec5439d6c812abbd4a0bbd5b6e9b379902e1
pedro <pedroaugusto.ufc@gmail..com>
2022-01-18 21:28:29 -0300

    Configuração inicial da página: arquivos estáticos css e js.

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<html><body>Melissa, te amo!!</body></html>')
+    return render(request, 'base/home.html')
